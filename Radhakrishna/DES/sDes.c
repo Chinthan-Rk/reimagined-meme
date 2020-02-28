b35dc2003e09f8dc6ada665fb167264b8420d481
Chinthan Rk <chinthanrkunder@gmail.com>
2020-02-28 22:15:48 +0530

    Update sDes.c

--- Radhakrishna/DES/sDes.c
+++ Radhakrishna/DES/sDes.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+int *keyGeneration(int key[],int P10[],int P8[])
+{
+    //XOR 10bit key with P10
+
+    int temp1[10];
+
+    for(int i=0;i<10;i++)
+    temp1[i] = key[i] ^ P10[i];
+
+    //split temp1 by 5bits and left shift
+}
+
 int main()
 {
 
@@ -16,7 +28,7 @@ int main()
     for(int i=0;i<10;i++)
     scanf("%d",key[i]);
 
-
+    int *keys = keyGeneration(key,P10,P8);
 
 
 }
