0e4cb271222f7b116b1113659214c50236f9b0d9
Chinthan Rk <chinthanrkunder@gmail.com>
2020-02-28 22:43:33 +0530

    Update sDes.c

--- Radhakrishna/DES/sDes.c
+++ Radhakrishna/DES/sDes.c
@@ -1,34 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int *keyGeneration(int key[],int P10[],int P8[])
+int *keyGeneration(int key[], int P10[], int P8[])
 {
     //XOR 10bit key with P10
 
-    int temp1[10];
+    int temp[10];
 
-    for(int i=0;i<10;i++)
-    temp1[i] = key[i] ^ P10[i];
+    for (int i = 0; i < 10; i++)
+        temp[i] = key[i] ^ P10[i];
 
     //split temp1 by 5bits and left shift
+
+    //Performing LS-1 on first half of temp
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == 4)
+            temp[i] = temp[0];
+        else
+            temp[i] = temp[i + 1];
+    }
+    //Performing LS-1 on second half of temp
+    for (int i = 5; i < 10; i++)
+    {
+        if (i == 9)
+            temp[i] = temp[5];
+        else
+            temp[i] = temp[i + 1];
+    }
 }
 
 int main()
 {
 
     int key[10];
-    int P10[10] ={
-                    3,5,2,7,4,10,1,9,8,6
-                 };
+    int P10[10] = {
+        3, 5, 2, 7, 4, 10, 1, 9, 8, 6};
 
     int P8 = {
-                6,3,7,4,8,5,10,9
-             };
+        6, 3, 7, 4, 8, 5, 10, 9};
 
     printf("Enter the 10bit key : ");
-    for(int i=0;i<10;i++)
-    scanf("%d",key[i]);
+    for (int i = 0; i < 10; i++)
+        scanf("%d", key[i]);
 
-    int *keys = keyGeneration(key,P10,P8);
-
-
+    int *keys = keyGeneration(key, P10, P8);
 }
